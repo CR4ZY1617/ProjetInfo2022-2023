b73c02d52a6eebc2abc1862ef3770d40688fa153
Yuliscqua <105552280+Yuliscqua@users.noreply.github.com>
2023-05-29 20:12:28 +0200

    Ajout save et read
    Ajout des fonctions de sauvegarde

--- main.c
+++ main.c
@@ -32,18 +32,13 @@ int MainPage(){
     }
     else if (choice == 1){
       go = 0;
-      //Ecrire dans le fichier save O pour remettre à 0 la sauvegarde.
-      // Ou le faire dans le fichier prologue.
     }
     else{
-      FILE *made = NULL;
-      made = fopen("save.txt", "r+");
-      if (fichier == NULL) {
-      printf("Ouverture du fichier impossible");
-      exit(1);
-      }
-      fscanf(made, "%d", &go);
-      fclose(made);
+      FILE * file = NULL;
+      file = fopen("save","rb+");
+      fread(&go,sizeof(int),1,file);
+      printf("%d \n",go);
+      fclose(file);
     }
   }
   return (go);
@@ -143,62 +138,95 @@ int read0(int go){
   scanf("%d", &go);
   return(go);
 }
-
+void save(int go){
+  FILE * file = NULL;
+  file = fopen("save","wb+");
+  scanf("%d",&go);
+  fwrite(&go,sizeof(int),1,file);
+  fclose(file);
+}
+  
 int list(int go){
   //Permet d'accéder à la liste des fichiers. 
   int take = -1;    
   switch(go){
-    case 0 : take = read0(go);
+    case 0 : save(go);
+      take = read0(go);
     break;
-    case 1 : take = lectureferme1(go);
+    case 1 : save(go);
+      take = lectureferme1(go);
     break;
-    case 2 : take = lecturecomi1(go);
+    case 2 : save(go);
+      take = lecturecomi1(go);
     break;
-    case 3 : take = lecturehopital1(go);
+    case 3 : save(go);
+      take = lecturehopital1(go);
     break;
-    case 4 : take = lecturecamp1(go);
+    case 4 : save(go);
+      take = lecturecamp1(go);
     break;
-    case 5 : take = lecturefuite(go);
+    case 5 : save(go);
+      take = lecturefuite(go);
     break;
-    case 6 : take = lectureferme2(go);
+    case 6 : save(go);
+      take = lectureferme2(go);
     break;
-    case 7 : take = lectureferme2bis(go);
+    case 7 : save(go);
+      take = lectureferme2bis(go);
     break;
-    case 8 : take = lecturefermebis2(go);
+    case 8 : save(go);
+      take = lecturefermebis2(go);
     break;
-    case 9 : take = lectureferme3(go);
+    case 9 : save(go);
+      take = lectureferme3(go);
     break;
-    case 10 : take = lecturecomi2(go);
+    case 10 : save(go);
+      take = lecturecomi2(go);
     break;
-    case 11 : take = lecturecomi2bis(go);
+    case 11 : save(go);
+      take = lecturecomi2bis(go);
     break;
-    case 12 : take = lecturecomi2bis2(go);
+    case 12 : save(go);
+      take = lecturecomi2bis2(go);
     break;
-    case 13 : take = lecturecomi2bis3(go);
+    case 13 : save(go);
+      take = lecturecomi2bis3(go);
     break;
-    case 14 : take = lecturecomi2bis4(go);
+    case 14 : save(go);
+      take = lecturecomi2bis4(go);
     break;
-    case 15 : take = lecturecomi3(go);
+    case 15 : save(go);
+      take = lecturecomi3(go);
     break;
-    case 16 : take = lecturehopitalgauche(go);
+    case 16 : save(go);
+      take = lecturehopitalgauche(go);
     break;
-    case 17 : take = lecturehopitaldroit1(go);
+    case 17 : save(go);
+      take = lecturehopitaldroit1(go);
     break;
-    case 18 : take = lecturehopitaldroit2(go);
+    case 18 : save(go);
+      take = lecturehopitaldroit2(go);
     break;
-    case 19 : take = lecturehopitaldroit2bis(go);
+    case 19 : save(go);
+      take = lecturehopitaldroit2bis(go);
     break;
-    case 20 : take = lecturehopitaldroit3(go);
+    case 20 : save(go);
+      take = lecturehopitaldroit3(go);
     break;
-    case 21 : take = lecturehopital3(go);
+    case 21 : save(go);
+      take = lecturehopital3(go);
     break;
-    case 22 : take = lecturecamp2(go);
+    case 22 : save(go);
+      take = lecturecamp2(go);
     break;
-    case 23 : take = lecturecamp3(go);
+    case 23 : save(go);
+      take = lecturecamp3(go);
     break;
-    case 24 : take = lecturemort();
+    case 24 : save(go);
+      take = lecturemort();
     break;
-    case 25 : take = lectureville(go);
+    case 25 : save(go);
+      take = lectureville(go);
     break;
     default : puts("Vous êtes hors des limites ! Mauvaise nouvelles, votre aventure s'arrête ici !");
     }
