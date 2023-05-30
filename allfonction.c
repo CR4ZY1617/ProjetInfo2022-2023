7a58c13b20a58a2f9f3f3031a26951c6260a1c68
CR4ZY1617 <105702795+CR4ZY1617@users.noreply.github.com>
2023-05-30 13:19:44 +0200

    Update allfonction.c

--- allfonction.c
+++ allfonction.c
@@ -52,3 +52,67 @@ int use_weapon(Chara a ,Ennemy b, int choice){
   }
   return(damage);
 }
+
+Ennemi ratb(){
+  //Lis le nom et les stats du rat dans son fichier.
+  Ennemi A;
+  FILE *test = NULL;
+  test = fopen("rat bléssé.txt", "r+");
+  if (test == NULL) {
+    printf("Ouverture du fichier impossible");
+    exit(1);
+  }
+  fscanf(test, "%s", A.nom );
+  fscanf(test, "%d", &A.pv );
+  fscanf(test, "%d", &A.dgts );
+  fclose(test);
+  return(A);
+}
+
+Ennemi rat(){
+  //Lis le nom et les stats du rat mutant dans son fichier.
+  Ennemi A; 
+  FILE *test = NULL;
+  test = fopen("rat.txt", "r+");
+  if (test == NULL) {
+    printf("Ouverture du fichier impossible");
+    exit(1);
+  }
+  fscanf(test, "%s", A.nom );
+  fscanf(test, "%d", &A.pv );
+  fscanf(test, "%d", &A.dgts );
+  fclose(test);
+  return(A);
+}
+
+Ennemi bandit(){
+  //Lis le nom et les stats du rat mutant dans son fichier.
+  Ennemi A; 
+  FILE *test = NULL;
+  test = fopen("bandit.txt", "r+");
+  if (test == NULL) {
+    printf("Ouverture du fichier impossible");
+    exit(1);
+  }
+  fscanf(test, "%s", A.nom );
+  fscanf(test, "%d", &A.pv );
+  fscanf(test, "%d", &A.dgts );
+  fclose(test);
+  return(A);
+}
+
+Ennemi goule(){
+  //Lis le nom et les stats du rat mutant dans son fichier.
+  Ennemi A; 
+  FILE *test = NULL;
+  test = fopen("goule.txt", "r+");
+  if (test == NULL) {
+    printf("Ouverture du fichier impossible");
+    exit(1);
+  }
+  fscanf(test, "%s", A.nom );
+  fscanf(test, "%d", &A.pv );
+  fscanf(test, "%d", &A.dgts );
+  fclose(test);
+  return(A);
+}
