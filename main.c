01bbdfffa9a78536b563df10b53a1a00542525e5
Yuliscqua <105552280+Yuliscqua@users.noreply.github.com>
2023-05-30 05:11:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -46,35 +46,7 @@ int MainPage(){
   return (go);
 } 
 
-int use_weapon(Chara a ,Ennemy b, int choice){
-  int damage;
-  if (choice == 1){
-    if (a.invent[1] <= 0 || a.invent[2] <= 0){
-      puts("Vous ne pouvez pas tirer avec cette arme !");
-    }
-    else{
-      damage = a.str + 5;
-    }
-  else if(choice == 2){
-    if (a.invent[3] <= 0){
-      puts("Vous n'avez pas cette arme !");
-    }
-    else{
-      damage = a.str + 2;
-    }
-  }
-  else{
-    if (a.invent[4] <= 0 || a.invent[5] <= 0){
-      puts("Vous ne pouvez pas tirer avec cette arme !");
-    }
-    else{
-      damage = a.str + 7;
-    }
-  }
-  return(damage);
-}
-  
-int combat(Chara a, Ennemy b){
+int combat(Heros a, Ennemy b){
   int end,answer,choice_w;
   while (a.hp > 0 && b.hp > 0){
     puts("Que voulez vous faire ? \n 1.Attaque au poing \n 2.Prendre une arme \n 3.Utiliser une trousse de soin");
@@ -140,20 +112,13 @@ int read0(int go){
   scanf("%d", &go);
   return(go);
 }
-void save(int go){
-  FILE * file = NULL;
-  file = fopen("save","wb+");
-  scanf("%d",&go);
-  fwrite(&go,sizeof(int),1,file);
-  fclose(file);
-}
-  
+
 int list(int go){
   //Permet d'accéder à la liste des fichiers. 
   int take = -1;    
   switch(go){
     case 0 : save(go);
-      take = read0(go);
+      take = lectureheroes(go);
     break;
     case 1 : save(go);
       take = lectureferme1(go);
@@ -234,24 +199,6 @@ int list(int go){
     }
   return(take);
 }
-
-void write_Heros(Heros a){
-  int val;
-  FILE * fichier = NULL;
-  fichier = fopen("Heros", "wb+");
-  fwrite(&a, sizeof(Heros), 1, fichier);
-  fclose(fichier);
-}
-
-Heros Initialise(){
-  //Lis le nom et les stats du héro dans son fichier.
-  Heros a;
-  FILE * fichier = NULL;
-  fichier = fopen("Heros", "rb+");
-  fread (&a,sizeof(Heros),1,fichier);
-  return(a);
-}
-
 
 int main(void) {
   int go = MainPage();
